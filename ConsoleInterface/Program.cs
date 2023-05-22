bbcb15227f4cf69e9d2b4876421ce4f060e982a4
zahrablend <zahra@LAPTOP-010BK67M>
2023-05-22 18:01:07 +0300

    Create project "ConsoleInterface" and project "Service"

---
+++ ConsoleInterface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
