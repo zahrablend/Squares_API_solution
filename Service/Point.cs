1e21d276713bcf71be5a17561d8f8ee55f1c4bf8
zahrablend <zahra@LAPTOP-010BK67M>
2023-05-25 10:56:08 +0300

    Create a list to store points. Create method to add points to list. Create method to delete points from list. Create method to print list of points.

--- Service/Point.cs
+++ Service/Point.cs
@@ -1,4 +1,7 @@
-﻿namespace Services
+﻿using System.Buffers.Text;
+using System.Drawing;
+
+namespace Services
 {
     // Define a struct to represent a point with x and y coordinates
     public struct Point
@@ -10,6 +13,40 @@
         {
             this.X = x;
             this.Y = y;
+        }
+    }
+
+    // Define a class to represent a list of points
+    public class PointList
+    {
+        // A List<Point> to store the points
+        private readonly List<Point> points;
+
+        // Constructor to initialize the list of points
+        public PointList()
+        {
+            points = new List<Point>();
+        }
+
+        // Method to add a point to the list
+        public void AddPoint(Point point)
+        {
+            points.Add(point);
+        }
+
+        // Method to delete a point from the list based on its x and y values
+        public void DeletePoint(int x, int y)
+        {
+            points.RemoveAll(point => point.X == x && point.Y == y);
+        }
+
+        // Method to print the list of points to the console
+        public void PrintPoints()
+        {
+            foreach (Point point in points)
+            {
+                Console.WriteLine("({0};{1})", point.X, point.Y);
+            }
         }
     }
 }
