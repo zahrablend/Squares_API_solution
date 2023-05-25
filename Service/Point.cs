877e75e825bd6f8703d1f8a9824b01160bcd9f92
zahrablend <zahra@LAPTOP-010BK67M>
2023-05-25 10:14:12 +0300

    Change project name to "Services". Add struct "Point" represented with coordinates x and y.

---
+++ Service/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace Services
+{
+    // Define a struct to represent a point with x and y coordinates
+    public struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
