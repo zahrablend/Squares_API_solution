535e081bd00dfa7b5da606fb86f290b7c5305124
zahrablend <zahra@LAPTOP-010BK67M>
2023-05-27 21:36:05 +0300

    Add user prompt options to 'ConsoleInterface' . Implement cases for adding and deleting points to the list, printing list of points, finding all squares in the list of points, printing squares count.

--- ConsoleInterface/Program.cs
+++ ConsoleInterface/Program.cs
@@ -1,10 +1,73 @@
 ﻿namespace ConsoleInterface
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create an instance of the PointService class
+            Services.PointService pointService = new();
+            Services.SquareService squareService = new(pointService);
+            bool exit = false;
+            while (!exit)
+            {
+                // Prompt the user to choose an operation
+                Console.WriteLine("Choose an operation:");
+                Console.WriteLine("1. Add point");
+                Console.WriteLine("2. Delete point");
+                Console.WriteLine("3. Save to JSON");
+                Console.WriteLine("4. Load from JSON");
+                Console.WriteLine("5. Print points");
+                Console.WriteLine("6. Find squares");
+                Console.WriteLine("7. Count squares");
+                Console.WriteLine("8. Exit");
+                string? input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        // Add a point to the list
+                        Console.Write("Enter x: ");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter y: ");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        pointService.AddPoint(new Services.PointService.Point(x, y));
+                        break;
+                    case "2":
+                        // Delete a point from the list based on its x and y values
+                        Console.Write("Enter x: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter y: ");
+                        y = Convert.ToInt32(Console.ReadLine());
+                        pointService.DeletePoint(x, y);
+                        break;
+                    case "3":
+                        // Save the list of points to a JSON file
+
+                    case "4":
+                        // Load the list of points from a JSON file
+
+                    case "5":
+                        // Print the list of points to the console
+                        pointService.PrintPoints();
+                        break;
+                    case "6":
+                        // Find all squares in the list of points and print their coordinates
+                        squareService.FindSquares();
+                        break;
+                    case "7":
+                        // Count and print the number of squares in the list of points
+                        int count = squareService.CountSquares();
+                        Console.WriteLine("{0} squares found", count);
+                        break;
+                    case "8":
+                        // Exit the program
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
         }
     }
 }
