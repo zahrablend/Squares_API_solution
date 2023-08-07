5c914687cdbeb9fd9c1d90a13660061c283d17fe
zahrablend <zahra@LAPTOP-010BK67M>
2023-08-07 13:43:36 +0300

    Update Point class in DataAccess; Update PointContext in DataAccess

--- DataAccess/Point.cs
+++ DataAccess/Point.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess
-{
-    internal class Point
+{ 
+    // Point entry class
+    public class Point
     {
+        [Key]
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
--- DataAccess/PointContext.cs
+++ DataAccess/PointContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    internal class PointContext
+    public class PointContext : DbContext
     {
+        // Representation of collection of points in a database
+        public DbSet<Point> Points { get; set;}
+
+        // Configuration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=my_db;Username=my_user;Password=my_pw");
+        }
     }
 }
